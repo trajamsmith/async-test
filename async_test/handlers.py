cc98a980b073fb4f0a9df570b8f5da6a9797da9a
Travis <trajamsmith@gmail.com>
2020-07-13 16:04:06 -0400

    Added a wait to the handler and it doesn't seem to block

--- async_test/handlers.py
+++ async_test/handlers.py
@@ -1,23 +1,29 @@
 import json
+import asyncio
+import time
 
 from notebook.base.handlers import APIHandler
 from notebook.utils import url_path_join
 import tornado
 
+
 class RouteHandler(APIHandler):
-    # The following decorator should be present on all verb methods (head, get, post, 
-    # patch, put, delete, options) to ensure only authorized user can request the 
+    # The following decorator should be present on all verb methods (head, get, post,
+    # patch, put, delete, options) to ensure only authorized user can request the
     # Jupyter server
     @tornado.web.authenticated
-    def get(self):
-        self.finish(json.dumps({
-            "data": "This is /async_test/get_example endpoint!"
-        }))
+    async def get(self):
+        start = time.time()
+        await asyncio.sleep(10)
+        end = time.time()
+
+        print(f"Handler took {round(end - start, 3)} seconds to return.")
+        self.finish(json.dumps({"data": "This is /async_test/get_example endpoint!"}))
 
 
 def setup_handlers(web_app):
     host_pattern = ".*$"
-    
+
     base_url = web_app.settings["base_url"]
     route_pattern = url_path_join(base_url, "async_test", "get_example")
     handlers = [(route_pattern, RouteHandler)]
